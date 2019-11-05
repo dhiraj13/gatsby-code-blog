e2699ba242b5f04f4d9704988e53693ae9f81f64
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-11-05 11:07:32 +0545

    Styling & Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Home page</h1>
   </Layout>
 )
