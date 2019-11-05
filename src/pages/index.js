51e314be2d48eb04af354748cffe2c605cfb7858
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-11-05 20:54:11 +0545

    Post Thumbnail Images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,65 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+import Post from '../components/Post'
+import { Row, Col } from 'reactstrap'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Home page</h1>
+    <Row>
+      <Col md="8">
+        <StaticQuery query={indexQuery} render={data => {
+          return (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Post
+                  title={node.frontmatter.title}
+                  author={node.frontmatter.author}
+                  path={node.frontmatter.path}
+                  date={node.frontmatter.date}
+                  body={node.excerpt}
+                  fluid={node.frontmatter.image.childImageSharp.fluid}
+                />
+              ))}
+            </div>
+          )
+        }}
+        />
+      </Col>
+      <Col md="4">
+        <div style={{width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.4)"}}></div>
+      </Col>
+    </Row>
   </Layout>
 )
+
+const indexQuery = graphql`
+query MyQuery {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMM Do YYYY")
+          author
+          path
+          image {
+            childImageSharp {
+              fluid(maxWidth: 600) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
