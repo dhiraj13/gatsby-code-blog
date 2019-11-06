823eaca6ed15a7c87d5eee308766b72721024845
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-11-06 14:15:57 +0545

    Refactoring Code

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
-  <Layout>
+  <Layout pageTitle="About us">
     <SEO title="About" keywords={[`gatsby`, `application`, `react` ]} />
-    <h1>About us..</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid a possimus obcaecati sapiente minima quos odio animi, molestiae rem ea reiciendis illum repudiandae eligendi beatae ut! Iste laudantium ab adipisci fuga aperiam quibusdam, earum animi laborum sed fugit facilis eos.</p>
   </Layout>
 )
 
